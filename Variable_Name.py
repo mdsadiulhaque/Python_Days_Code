2e2eb14316423f6f902dd370a2e649b4ac79e857
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-03-25 06:41:12 +0600

    Python Variable Name

---
+++ Variable_Name.py
@@ -0,0 +1,14 @@
+myvar = "Sadiul"
+my_var = "Sadiul"
+_my_var = "Sadiul"
+myVar = "Sadiul"
+MYVAR = "Sadiul"
+myvar2 = "Sadiul"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
