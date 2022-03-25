80ec192673d8e07efdc32cfe04536995a76a22ea
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-03-25 06:37:21 +0600

    python variable

---
+++ variable.py
@@ -0,0 +1,10 @@
+from tkinter import N
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+xx = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(xx,y,z)
