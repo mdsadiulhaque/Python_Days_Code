29a35c5369f0f62ab57e29986f1866936d8617bd
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-04-03 17:48:13 +0600

    Update Paython_Number.py

--- Paython_Number.py
+++ Paython_Number.py
@@ -8,3 +8,18 @@ print(type(x))
 print(type(y))
 print(type(z))
 
+a = float(1)
+
+#convert from float to int:
+b = int(2.8)
+
+#convert from int to complex:
+c = complex(a)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
