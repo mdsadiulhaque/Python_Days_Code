79ba82527e9af0f39b302df776ee687f4f332cbd
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-04-05 01:04:59 +0600

    Create Access_Tuple.py

---
+++ Access_Tuple.py
@@ -0,0 +1,10 @@
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+print(thistuple[-1])
+print(thistuple[1])
+print(thistuple[:4])
+print(thistuple[2:])
+print(thistuple[-4:-1])
+
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
