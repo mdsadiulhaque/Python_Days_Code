a0fa89b32d5ff8c652d049e36415041570f1b81e
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-04-06 00:37:17 +0600

    Create While_loop.py

---
+++ While_loop.py
@@ -0,0 +1,20 @@
+i =1
+while i < 6:
+    print(i)
+    i += 1
+    
+    j = 1
+while j < 6:
+  print(j)
+  if j == 3:
+    break
+  j += 1
+  
+  z = 1
+while z < 6:
+  print(z)
+  z += 1
+else:
+  print("z is no longer less than 6")
+
+  
