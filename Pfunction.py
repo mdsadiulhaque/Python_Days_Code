f9ec5adbca27767b112b291a3b18a12784a0fb32
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-04-06 01:12:18 +0600

    Create Pfunction.py

---
+++ Pfunction.py
@@ -0,0 +1,15 @@
+def my_function(sadiul):
+ print(sadiul + " haque")
+my_function("BigFokir")
+my_function("Fokir")
+my_function("shf")
+
+
+def my_function(child3, child2, 
+                child1):
+      print("The youngest child is " 
+            + child3)
+
+my_function(child1 = "Emil", 
+            child2 = "Tobias", 
+            child3 = "Linus")
